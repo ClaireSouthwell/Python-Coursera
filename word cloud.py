018b6e6cb0a55488f22254a8f364b52263a33b11
Claire Southwell <47905732+ClaireSouthwell@users.noreply.github.com>
2021-02-02 14:05:22 -0500

    Word cloud generator
    Version submitted for use with Jupyter Notebooks.

---
+++ word cloud.py
@@ -0,0 +1,60 @@
+# Here are all the installs and imports you will need for your word cloud script and uploader widget
+'''
+!pip install wordcloud
+!pip install fileupload
+!pip install ipywidgets
+!jupyter nbextension install --py --user fileupload
+!jupyter nbextension enable --py fileupload
+'''
+import wordcloud
+import numpy as np
+from matplotlib import pyplot as plt
+from IPython.display import display
+import fileupload
+import io
+import sys
+
+# This is the uploader widget
+
+def _upload():
+
+    _upload_widget = fileupload.FileUploadWidget()
+
+    def _cb(change):
+        global file_contents
+        decoded = io.StringIO(change['owner'].data.decode('utf-8'))
+        filename = change['owner'].filename
+        print('Uploaded `{}` ({:.2f} kB)'.format(
+            filename, len(decoded.read()) / 2 **10))
+        file_contents = decoded.getvalue()
+
+    _upload_widget.observe(_cb, names='data')
+    display(_upload_widget)
+
+_upload()
+
+def calculate_frequencies(file_contents):
+    # Here is a list of punctuations and uninteresting words you can use to process your text
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    uninteresting_words = ["in", "into", "then", "on", "there", "for", "said", "up", "the", "out", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", \
+    "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its", "they", "them", \
+    "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be", "been", "being", \
+    "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how", \
+    "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just"]
+    
+    # LEARNER CODE START HERE
+    all_words = file_contents.split()
+    clean_words = {}
+    for word in all_words:
+        word = word.lower().strip('!()-[]{};:\'"\,<>./?@#$%^&*_~')
+        if word in uninteresting_words:
+            continue
+        elif word in clean_words:
+            clean_words[word] += 1
+        else:
+            clean_words[word] = 1
+            
+    #wordcloud
+    cloud = wordcloud.WordCloud()
+    cloud.generate_from_frequencies(clean_words)
+    return cloud.to_array()
