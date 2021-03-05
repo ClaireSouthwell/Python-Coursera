0352f5459653bd7bf7ba56011773373c6d0dc0f3
Claire Southwell <47905732+ClaireSouthwell@users.noreply.github.com>
2021-03-05 16:55:05 -0500

    Final version
    Unsure about syntax for regex groups on lines 21-23

--- ticky_check.py
+++ ticky_check.py
@@ -13,61 +13,52 @@ reg = r'ticky: (\w{4,5}) ([\w ]*) (\[#\d+\]\s)*\(([\w.]+)\)'
 per_user = {}
 error = {}
 
-#SYSLOG FILE
-syslog =r"/home/student-00-565cdf4a2ebb/syslog.log"
 
-def get_stats(logfile):
 # Reading lines of the log file:
-    with open(logfile, 'r') as file:
-        for line in file.readlines():
-            result = re.search(reg, line)
-            name = result.group(4)
-            ie = result.group(1)
-            msg = result.group(2)
+with open('syslog.log', 'r') as f:
+    for line in f:
+        result = re.search(reg, line)
+        name = result.groups()[4]
+        ie = result.groups()[1]
+        msg = result.groups()[2]
         # Add ALL messages to user statistics dictionary
-            if name in per_user:
-                if ie in per_user[name]:
-                    per_user[name][ie] += 1
-                else:
-                    per_user[name][ie] = 1
+        if name in per_user:
+            if ie in per_user[name]:
+                per_user[name][ie] += 1
             else:
-                per_user[name] = {ie: 1}
+                per_user[name][ie] = 1
+        else:
+            per_user[name] = {ie: 1}
         # Add errors to error dictionary 
-            if ie == 'ERROR':
-                if msg in error:
-                    error[msg] += 1
-                else:
-                    error[msg] = 1
+        if ie == 'ERROR':
+            if msg in error:
+                error[msg] += 1
+            else:
+                error[msg] = 1
             
 
 # Sort the dictionaries
-    error = sorted(error.items(), key=operator.itemgetter(1), reverse=True)
-    per_user = sorted(per_user.items())
+error = sorted(error.items(), key=operator.itemgetter(1), reverse=True)
+per_user = sorted(per_user.items())
 
-    return per_user, error
 
-def make_csv(per_user, error):
 # Write per_user to user_statistics.csv
-    user_keys = ["Username", "INFO", "ERROR"]
-    with open ('user_statistics.csv', 'w') as user_csv:
-        writer = csv.writer(user_csv, fieldnames=user_keys)
-        writer.writeheader()
-        for item in per_user:
-            user, log_type = item
-            line = [user,log_type["INFO"],log_type["ERROR"]]                 
-            writer.writerow(line)
+user_keys = ["Username", "INFO", "ERROR"]
+with open ('user_statistics.csv', 'w') as user_csv:
+    writer = csv.writer(user_csv, fieldnames=user_keys)
+    writer.writeheader()
+    for item in per_user:
+        user, log_type = item
+        line = [user,log_type["INFO"],log_type["ERROR"]]                 
+        writer.writerow(line)
     
 # Write error to error_message.csv
-    error_keys = ["Error", "Count"]
-    with open ('error_message.csv', 'w') as error_csv:
-        writer = csv.writer(error_csv, fieldnames=error_keys)
-        writer.writeheader()
-        writer.writerows(error)
-                    
+error_keys = ["Error", "Count"]
+with open ('error_message.csv', 'w') as error_csv:
+    writer = csv.writer(error_csv, fieldnames=error_keys)
+    writer.writeheader()
+    writer.writerows(error)
+    
 
-if __name__ == '__main__':
-    per_user, error = get_stats(syslog)
-    make_csv(per_user, error)
-                    
-                    
+              
 
